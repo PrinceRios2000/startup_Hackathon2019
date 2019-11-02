7975151bea192b5e09dcc7878693e8089f40cf15
Ubuntu <ubuntu@ip-172-31-43-189.ec2.internal>
2019-11-02 19:17:17 +0000

    added user's reports logic

--- main.py
+++ main.py
@@ -15,19 +15,40 @@ def checkLoggedInAndRegistered(request):
     if not profile:
         request.redirect("/login")
         return
-    email_address = profile.nickname()
-    registered_profile = User.query().filter(User.email == email_address).get()
-    if not registered_profile:
+    emailAddress = profile.nickname()
+    registeredProfile = User.query().filter(User.email == emailAddress).get()
+    if not registeredProfile:
         request.redirect("/register")
         return
-class Home(webapp2.RequestHandler):
+
+class Reports(webapp2.RequestHandler):
     def get(self):
         checkLoggedInAndRegistered(self)
         
         complaintPosts = Complaints.query()
         
+        postLogoutLogic = {
+            "logoutUrl": users.create_logout_url('/'),
+            "complaints": complaintPosts
+        }
         
+        home_template = the_jinja_env.get_template('templates/main.html')
+        self.response.write(home_template.render(postLogoutLogic))
         
+    def post(self):
+        checkLoggedInAndRegistered(self)
+        
+        profile = users.get_current_user()
+        
+        post=Complaints(
+            Type = self.request.get('Type1st'),
+            Location = self.request.get('Location2nd')
+            Date = datetime.datetime.fromtimestamp(time.mktime(utc_time.timetuple())
+            )
+        post_key = post.put()
+        self.response.write("Reports created: " + str(post_key) + "<br>")
+        self.response.write("<a href='/'>Home</a> | ")
+        self.response.write("<a href='/myreport'>My posts</a>")
         
 
 
--- models.py
+++ models.py
@@ -9,4 +9,5 @@ class User(ndb.model):
     FirstName = ndb.StringProperty(required=True)
     LastName = ndb.StringProperty(required=True)
     Email = ndb.Email(required=True)
+    PhoneNumber = ndb.PhoneNumberProperty(required=True)
     Location = ndb.StringProperty(required=True)
