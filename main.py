c349694117e503fef779b349bd5ea2dc1d17e69f
Ubuntu <ubuntu@ip-172-31-43-189.ec2.internal>
2019-11-02 17:26:40 +0000

    added handker 'Home'

--- main.py
+++ main.py
@@ -3,22 +3,38 @@ import jinja2
 import os
 from google.appengine.api import users
 from google.appengine.ext import db
-from models import 
+from models import Complaints, User
 
 the_jinja_env = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
     extensions=['jinja2.ext.autoescape'],
     autoescape=True)
     
-def checkLoggedInAndRegistered(request){
+def checkLoggedInAndRegistered(request):
     profile = users.get_current_user()
     if not profile:
         request.redirect("/login")
         return
     email_address = profile.nickname()
-    registered_profile = Profile.query().filter(Profile.email == email_address).get()
+    registered_profile = User.query().filter(User.email == email_address).get()
     if not registered_profile:
         request.redirect("/register")
         return
+class Home(webapp2.RequestHandler):
+    def get(self):
+        checkLoggedInAndRegistered(self)
+        
+        complaintPosts = Complaints.query()
+        
+        
+        
+        
+
+
     
-}
+app = webapp2.WSGIApplication([
+    ('/', Home),
+    ('/reports', Reports),
+    ('/myreport', userReport),
+    ('register', Registration)
+], debug=True)
